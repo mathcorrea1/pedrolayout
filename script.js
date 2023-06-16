065244a6a897d8e5aff89c66ccfda641f31e1900
Matheus <math_10correa@hotmail.com>
2023-06-16 09:20:28 -0300

    1606

---
+++ script.js
@@ -0,0 +1,8 @@
+function abrirModal() {
+    document.getElementById("modalCadastro").classList.add("is-active");
+  }
+  
+  function fecharModal() {
+    document.getElementById("modalCadastro").classList.remove("is-active");
+  }
+  
